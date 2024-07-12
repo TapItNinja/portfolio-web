d0bfe1617708270d57d5e1e380297791b8f23f72
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2024-07-12 14:54:05 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "S";
+const meta = {
+    title: "Shubham Shaswat",
+    description: "I’m Shubham Shaswat data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Shubham Shaswat",
+    animated: {
+        first: "I thrive on mastering DSA.",
+        second: "DBMS and SQL are my forte.",
+        third: "Data analysis fuels my creativity.",
+    },
+    description: "As a fourth-year student at VIT Vellore, I am passionate about leveraging technology and data to drive innovative solutions.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Introduction",
+    aboutme: "I’m a final-year student at VIT, passionate about data analysis, web development, and database management. My studies have given me strong analytical and technical skills. I love solving complex problems and driving innovation through technology. Always eager to learn and grow, I’m excited to apply my knowledge in real-world settings. Looking forward to new opportunities to make a positive impact.",
+};
+const worktimeline = [{
+        jobtitle: "Data Analyst Intern",
+        where: "State Bank of India",
+        date: "12/2023 - 01/2024",
+    },
+    {
+        jobtitle: "IT Intern",
+        where: "Paisalo",
+        date: "08/2023 - 09/2023",
+    },
+    {
+        jobtitle: "Front-End Intern",
+        where: "Jindal Steel and Power",
+        date: "03/2023 - 05/2023",
+    },
+];
+
+const skills = [{
+        name: "Vellore Institute of Technology (2025)",
+        value: 84.5,
+    },
+    {
+        name: "12th (HSc.)",
+        value: 84.8,
+    },
+    {
+        name: "10th",
+        value: 88.6,
+    },
+    // {
+    //     name: "React",
+    //     value: 60,
+    // },
+    // {
+    //     name: "Jquery",
+    //     value: 85,
+    // },
+];
+
+const services = [{
+        title: "Chat Application using React and Firebase (2023)",
+        description: "Developed a responsive chat application using React to ensure seamless user experience. Integrated Firebase for real-time database functionalities, enabling instant message updates.",
+    },
+    {
+        title: "Movie Recommendation System (2023)",
+        description: "Created a Movie Recommendation System using a Kaggle dataset, employing machine learning techniques to personalize movie suggestions based on user preferences.",
+    },
+    {
+        title: "Tetris Game using C++ and Raylib (2024)",
+        description: "Developed a Tetris game using C++, demonstrating proficiency in object-oriented programming and game development principles.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Web Chat Application",
+        link: "https://github.com/TapItNinja/chat-app",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Movie Recommender",
+        link: "https://github.com/TapItNinja/movie--recommender",
+    },
+    {
+            img: "https://picsum.photos/400/600/?grayscale",
+            description: "The wisdom of life consists in the elimination of non-essentials.",
+            link: "#",
+        },
+        {
+            img: "https://picsum.photos/400/?grayscale",
+            description: "The wisdom of life consists in the elimination of non-essentials.",
+            link: "#",
+        },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shubham.shaswat2002@gmail.com",
+    YOUR_FONE: "+91 8076344502",
+    description: "Contact me for collaborations, opportunities, or inquiries.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_vd6ttki",
+    YOUR_TEMPLATE_ID: "template_hbbeebk",
+    YOUR_USER_ID: "1WRCt7hgR1OT7RrPb",
+};
+
+const socialprofils = {
+    github: "https://github.com/tapitninja",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/shubham-shaswat-1b3369211/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
